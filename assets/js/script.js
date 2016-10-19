d0c1742de3660b61efa0dedf93a015f0c27c0cb7
Ire Aderinokun <ire@ireaderinokun.com>
2016-10-19 19:05:25 +0100

    slideshow pages

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+
+function initialiseSlideshow() {
+
+    const slideElements = Array.from( document.querySelectorAll('[data-slide]') );
+    let index = 0;
+
+    setInterval(function() {
+        if ( index === (slideElements.length - 1) ) index = 0
+        let imageUrl = slideElements[index].dataset.slide;
+        slideshowBackground.style.backgroundImage = `url(${imageUrl})`;
+        index++
+    }, 3000);
+
+}
+
+const slideshowBackground = document.querySelector('.slideshow-background');
+if ( slideshowBackground ) {
+    initialiseSlideshow();
+}
