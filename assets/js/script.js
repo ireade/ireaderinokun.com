b44e9461926eb1c137c54579bbd9492a932094d3
Ire Aderinokun <ire@ireaderinokun.com>
2016-10-24 14:03:39 +0100

    nav icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 
+/* Slideshow Page ****************** */
+
 function initialiseSlideshow() {
 
     const slideElements = Array.from( document.querySelectorAll('[data-slide]') );
@@ -17,3 +19,12 @@ const slideshowBackground = document.querySelector('.slideshow-background');
 if ( slideshowBackground ) {
     initialiseSlideshow();
 }
+
+
+/* Nav ****************** */
+
+const navButton = document.querySelector('.nav-icon');
+navButton.addEventListener('click', function(e) {
+    e.preventDefault();
+    console.log("clicked");
+})
