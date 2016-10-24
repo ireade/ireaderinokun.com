811e7a8e3de6d4471885269c29f3c88adfde656a
Ire Aderinokun <ire@ireaderinokun.com>
2016-10-24 19:48:40 +0100

    work page, macbooks

--- assets/js/script.js
+++ assets/js/script.js
@@ -96,26 +96,7 @@ var EPPZScrollTo =
 
 
 
-/* Slideshow Page ****************** */
 
-function initialiseSlideshow() {
-
-    const slideElements = Array.from( document.querySelectorAll('[data-slide]') );
-    let index = 0;
-
-    setInterval(function() {
-        if ( index === (slideElements.length - 1) ) index = 0
-        let imageUrl = slideElements[index].dataset.slide;
-        slideshowBackground.style.backgroundImage = `url(${imageUrl})`;
-        index++
-    }, 3000);
-
-}
-
-const slideshowBackground = document.querySelector('.slideshow-background');
-if ( slideshowBackground ) {
-    initialiseSlideshow();
-}
 
 
 /* Nav ****************** */
