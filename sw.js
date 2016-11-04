6bc4f29117db3f901cba55e534438107e163c6ea
Ire Aderinokun <ire@ireaderinokun.com>
2016-11-04 19:19:47 +0100

    writing

--- sw.js
+++ sw.js
@@ -17,6 +17,12 @@ self.addEventListener('install', (event) => event.waitUntil(self.skipWaiting()))
 self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()));
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then((response) => response || fetch(event.request))
+        caches.match(event.request).then((response) => {
+            if ( response ) {
+                return response;
+            } else {
+                return fetch(event.request);
+            }
+        })
     );
 });
